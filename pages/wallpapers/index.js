9b345e426c6276a388e037765e6849bc25884ad7
roeintheglasses <hrkjangir@gmail.com>
2023-04-18 20:38:19 +0530

    updated image size

--- pages/wallpapers/index.js
+++ pages/wallpapers/index.js
@@ -123,8 +123,8 @@ function Wallpapers({ images }) {
                 placeholder="blur"
                 blurDataURL={blurDataUrl}
                 src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-                width={720}
-                height={480}
+                width={400}
+                height={840}
                 sizes="(max-width: 640px) 100vw,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 33vw,
